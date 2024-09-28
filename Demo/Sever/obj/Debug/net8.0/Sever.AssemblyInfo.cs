888ec2d25c0c646733e913b38d40161cf0bea732
Nguyễn Phước Tự Tin <90522812+phomaizq@users.noreply.github.com>
2024-09-29 01:58:10 +0700

    merger

--- Demo/Sever/obj/Debug/net8.0/Sever.AssemblyInfo.cs
+++ Demo/Sever/obj/Debug/net8.0/Sever.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sever")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a128d0aabb7ff8f89421089fe319fb58fa29351c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e4521f495040ea9463f15231aeb04bfb08b04e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sever")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sever")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
